faff42eca5014fd3421fc8312a9d5f678aafa7e8
Nguyen Ngoc Thien <thienitcoder2004@gmail.com>
2025-03-25 13:06:44 +0700

    [add] lab05 base64

---
+++ lab05/base64/encrypt.py
@@ -0,0 +1,12 @@
+import base64
+
+def main():
+    input_string = input("Nhap thong tin can ma hoa:")
+    encoded_bytes = base64.b64encode(input_string.encode('utf-8'))
+    encoded_string = encoded_bytes.decode('utf-8')
+    with open("data.txt", "w") as file:
+        file.write(encoded_string)
+    print("Da duoc ma hoa va luu vao file data.txt")
+
+if __name__ == "__main__":
+    main()
